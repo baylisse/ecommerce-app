63b8efc8a458b798c3d92af0749f918025725899
Betsy Bayliss <betsy.bayliss@multiverse.io>
2023-01-04 09:23:01 -0800

    adds addToCart functionality and item dropdown component

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
 import Button from '../button/Button.js';
+import CartItem from '../cart-item/CartItem.js';
+import { CartContext } from '../../contexts/CartContext.js';
+
 import './CartDropdown.styles.scss';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
+            <div className='cart-items'>
+                { cartItems.map((item, index) => <CartItem key={index} cartItem={item} />)}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
